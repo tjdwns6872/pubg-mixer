fe5abfff77aa4253c2b793af058ea94e7ba82c0e
tjdwns6872 <tjdwns6872@naver.com>
2026-03-09 11:12:50 +0900

    chore: 로깅 시스템(Logback) 구축 및 JPA Auditing 활성화

---
+++ pubg-mixer/backend/src/main/java/com/pubg/mixer/backend/config/JpaAuditConfig.java
@@ -0,0 +1,10 @@
+package com.pubg.mixer.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditConfig {
+    
+}
